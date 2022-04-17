d75e7a9f413d2111efdc00080c2f04b442de35b8
Cesar Emmanuel <cesaremmmanul@gmail.com>
2022-04-17 13:03:20 -0300

    Criando estilos globais

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-import styled from 'styled-components'
+import { GlobalStyle } from './styles/global'
 
 export function App() {
   return (
-    <div className="App">
+    <div>
       <h1>Hello world</h1>
+
+      <GlobalStyle />
     </div>
   )
 }
